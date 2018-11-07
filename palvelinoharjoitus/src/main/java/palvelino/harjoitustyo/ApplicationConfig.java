6903602ef1b0e758ba0908beab132ee145e06aeb
Kvartti <a1704516@myy.haaga-helia.fi>
2018-11-07 14:50:13 +0200

    i18n testi

--- palvelinoharjoitus/src/main/java/palvelino/harjoitustyo/ApplicationConfig.java
+++ palvelinoharjoitus/src/main/java/palvelino/harjoitustyo/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package palvelino.harjoitustyo;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -26,7 +29,25 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter {
 	       lci.setParamName("lang");
 	       return lci;
 	   }
-	 
+	   
+	/*   @Bean
+	    public MessageSource messageSource() {
+	        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	        messageSource.setBasename("message");
+	        messageSource.setDefaultEncoding("UTF-8");
+	        return messageSource;
+	    } */
+	   
+	   /*   @Bean
+	   public MessageSource messageSource() {
+	       final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+	       messageSource.setBasenames("classpath:/some-mvc-messages", "classpath:/some-other-mvc-messages", "classpath:/another-projects/mvc-messages");
+	       messageSource.setUseCodeAsDefaultMessage(true);
+	       messageSource.setDefaultEncoding("UTF-8");
+	       messageSource.setCacheSeconds(5);
+	       return messageSource;
+	   }*/
+	   
 	   @Override
 	   public void addInterceptors(InterceptorRegistry registry) {
 	       registry.addInterceptor(localeChangeInterceptor());
